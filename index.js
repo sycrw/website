ae414fe92c8d4f217ce6d8ae3696391c97f17a64
sycrw <timmartinkausemann@mail.com>
2022-10-07 00:18:10 +0200

    projects section

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 
 window.addEventListener('scroll', move_logo);
 window.addEventListener('scroll', move_description);
+ 
 
 function move_logo() {
+    var progress = (window.pageYOffset);
+
     const welcome_logo = document.querySelector('#sycrw');
-     var progress = (window.pageYOffset);
-     
-     var input = progress + "px";
-     
+    var input = ((4*progress)) + "px"; 
     welcome_logo.style.left = input;
-    
-    
+     
  }
  
 function move_description(){
     const welcome_description = document.querySelector('#sycrw_des');
-     var progress = (-window.pageYOffset);
+     var progress = (window.pageYOffset);
      
-     var input = progress + "px";
+     var input = (-4*progress) + "px";
      
     welcome_description.style.left = input;
+    
     
     
 
