0fc1f14e9906292f7cb1d59f06649e1310d92cfe
sycrw <114520804+sycrw@users.noreply.github.com>
2022-12-25 16:42:28 +0000

    shfurefr

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+addEventListener("scroll", (event) => {
+    manageNavbarOnScroll();
+});
+function manageNavbarOnScroll(){
+    let navbar = document.getElementById("navbar");
+    if(scrollY<=10){
+        navbar.className = "navbar_shown";
+    }
+    else{
+        navbar.className = "navbar_hidden";
+    }
+}
 
