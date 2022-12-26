f10165a9c2fb128ca464bd517a4240c41d704e8a
sycrw <114520804+sycrw@users.noreply.github.com>
2022-12-26 10:28:26 +0000

    about section started

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-addEventListener("scroll", (event) => {
-    manageNavbarOnScroll();
-});
-function manageNavbarOnScroll(){
-    let navbar = document.getElementById("navbar");
-    if(scrollY<=10){
-        navbar.className = "navbar_shown";
-    }
-    else{
-        navbar.className = "navbar_hidden";
-    }
-}
+
 
