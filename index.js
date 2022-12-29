82287c2da2494e656905edc657f4e3733c5bfaac
sycrw <timmartinkausemann@mail.com>
2022-12-29 14:30:26 +0100

    coomit from windows

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
+let navbarOn;
+function handleNavbarClick(){
+    let navbar = document.getElementById("navbar");
+    navbar.style.backgroundColor = "rgb(0,0,0,0);
+}
+function init(){
+    if(window.innerWidth == 1000){
+        navbarOn = false;
+    }
+    else
+}
 
